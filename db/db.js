4cfa47de654ca4a34f750711d81e0bf6c6c4319d
deepsaia <140556219+deepsaia@users.noreply.github.com>
2025-09-23 18:17:55 -0700

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,233 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Deepak Singh. I’m Master's in Data Science student at the University of San Francisco and currently working as a Data Science Intern at Boston Children's Hospital (Harvard Medical School). I'm graduating in June 2023.",
+				"I'm a firm believer of the idea that 'data' is a catalyst to a 'better world'. I've been a bridge between technology and business, delivering solutions for interesting problems in a wide spectrum of industry space.",
+				"I'm a 3d artist at heart, besides being an OpenSource enthusiast. Look me up at Sketchfab (10k+ views) and Itch.io",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: deepak.id1988@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages & DBs",
+			skillName: "Python, SQL, Apache Spark, Airflow, MongoDB",
+			color: "1",
+			percentage: "100",
+		},
+		{
+			title: "Libraries",
+			skillName: "numpy, pandas, matplotlib, sklearn, Git",
+			color: "2",
+			percentage: "100",
+		},
+		{
+			title: "Frameworks",
+			skillName: "LR, ML, A/B Testing, Scraping, SVD",
+			color: "3",
+			percentage: "100",
+		},
+		{
+			title: "Cloud Platform",
+			skillName: "GCP, Databricks",
+			color: "4",
+			percentage: "100",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Quizzing App",
+				image: "images/quizapp_icon.jpg",
+				summary: "Developed a quizzing application that provides a gamified interface for students on any topic",
+				preview: "https://github.com/deepsAI8/Quizapp",
+				techStack: ["Python","SQLite",],
+			},
+			{
+				projectName: "Cost Estimation",
+				image: "images/dsce_icon.jpg",
+				summary: "Developed a Cost estimation tool for project cost in the piping engineering industry",
+				preview: "https://github.com/deepsAI8/Estimator-Code-Snippets",
+				techStack: ["Python","SQLite","javascript",],
+			},
+		],
+		software: [
+			{
+				projectName: "Open-Biking",
+				image: "images/open_biking_512x512_icon.png",
+				summary: "An android app for bike hiring at campus (Developed as part of Creating and Managing New Ventures against 11 other competing ventures)",
+				preview: "https://github.com/deepsAI8/Open-Biking",
+				techStack: ["C#", "Android", "Python",],
+			},
+		],
+		android: [
+			{
+				projectName: "Open-Biking",
+				image: "images/open_biking_512x512_icon.png",
+				summary: "An android app for bike hiring at campus (Developed as part of Creating and Managing New Ventures against 11 other competing ventures)",
+				preview: "https://github.com/deepsAI8/Open-Biking",
+				techStack: ["C#", "Android", "Python",],
+			},
+		],
+		freelance: [
+			{
+				projectName: "Open-Biking",
+				image: "images/open_biking_512x512_icon.png",
+				summary: "An android app for bike hiring at campus (Developed as part of Creating and Managing New Ventures against 11 other competing ventures)",
+				preview: "https://github.com/deepsAI8/Open-Biking",
+				techStack: ["C#", "Android", "Python",],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Boston Children's Hospital",
+			duration: "Nov 2022 - Present",
+			subtitle: "Data Science Intern",
+			details: [
+				"Working on deriving and implementing (in Python) a coupled tensor+matrix factorization algorithm (SupCP+M) and then to test this algorithm with a multimodal neurodevelopmental dataset that includes EEG, several clinical measures (e.g., birthweight, sex), sociodemographic indicators, and genetic data.",
+			],
+			tags: [
+				"Python",
+				"Tensor-Matrix Factorization",
+				"SVD",
+				"Time Series",
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Accenture",
+			duration: "Apr 2018 - Jun 2022",
+			subtitle: "Business Excellence Advisory",
+			details: [
+				"Deployed a predictive performance model for over 40k partners of a billion dollar client by back-end automation, which enabled B2B partners to spend more time in selling rather than managing compensation",
+				"Designed a low-cost online workflow system, which transformed E2E contract management for 5000+ customers, converting unstructured contract data into meaningful insights",
+				"As a product owner, spearheaded ML based classification model, which detects erroneous/non-billable claims, which improved Working Capital by 10% (~$ 1.6 M) y-o-y",
+			],
+			tags: [
+				"Predictive Modeling",
+				"SQL",
+				"Python",
+				"RPA",
+				"Power Apps",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "Hindustan Petroleum Corporation Ltd.",
+			duration: "Jun 2011 - Apr 2017",
+			subtitle: "Senior Engineer",
+			details: [
+				"Achieved savings of $70k annually by digitizing and implementing an automated invoice validation system to reduce the validation time from 3 days to less than a day",
+				"Transformed monitoring & controlling of capital budget projects worth $500 k, across the organization by developing a standard monitoring tool ",
+			],
+			tags: ["Excel", "Automation", "SQL",],
+			icon: "group",
+		},
+		{
+			title: "Cognizant Technology Solutions",
+			duration: "Feb 2010 - Jun 2011",
+			subtitle: "Programmer Analyst",
+			details: [
+				"Implemented Application Quality Monitoring (API) app, which improved overall product quality to 95% for one of the major insurers in the USA",
+				"Developed a python tool for converting lengthy word docs into page level pdf files with dynamic titles based on page content",
+			],
+			tags: ["Python", "Automation", "Quality",],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Master's in Data Science",
+			duration: "",
+			subtitle: "University of San Francisco, California",
+			details: ["Focus on A/B Testing, Machine Learning, Distributed Computing",],
+			tags: ["A/B Testing","Machine Learning","Distributed Computing",],
+			icon: "graduation-cap",
+		},
+		{
+			title: "MBA",
+			duration: "",
+			subtitle: "Indian Institute of Management, Calcutta",
+			details: ["Led the campus team and won 2nd prize at ISB’s Nimble case challenge by Tech Mahindra among 1200+ teams",
+				"Developed Android app for an entrepreneurial bike rides venture across the campus, earned highest revenues among 11 competing ventures",
+			],
+			tags: ["Operations Research","Economics","Marketing",],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Bachelors of Technology",
+			duration: "",
+			subtitle: "Maharshi Dayanand University",
+			details: ["Mechanical Engineering","Technical events organizer",],
+			tags: ["Computer Aided Design", "Automation",],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/deepsAI8",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/dy1988",
+				},
+				{
+					text: "HackerRank",
+					link: "https://www.hackerrank.com/deepak_id1988",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "Clone this page",
+					link: "https://github.com/deepsAI8/deepsAI8.github.io",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/deepaksingh8819/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/dsy1988",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/deepsai8",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Deepak Singh.",
+				"&copy; No Copyrights. Feel free to use this template.",
+			],
+		},
+	],
+};
